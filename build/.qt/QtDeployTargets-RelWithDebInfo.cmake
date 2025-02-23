99eb5736d93e12c5402f64f72a8d7c65ce26de90
Karolina <karoli.vallo@gmail.com>
2025-02-23 17:34:02 +0100

    Add project files.

---
+++ build/.qt/QtDeployTargets-RelWithDebInfo.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_ImageViewerSO_FILE C:/Users/karol/Documents/UNI_SCHOOL/STU/Inzinier/Semester_8/SpracovanieObrazu/ImageViewerSO/build/RelWithDebInfo/ImageViewerSO.exe)
+set(__QT_DEPLOY_TARGET_ImageViewerSO_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_ImageViewerSO_RUNTIME_DLLS C:/Qt/6.8.2/msvc2022_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/msvc2022_64/bin/Qt6Core.dll)
---
+++ src/ImageViewer.cpp
@@ -0,0 +1,130 @@
+#include "ImageViewer.h"
+
+ImageViewer::ImageViewer(QWidget* parent)
+	: QMainWindow(parent), ui(new Ui::ImageViewerClass)
+{
+	ui->setupUi(this);
+	vW = new ViewerWidget(QSize(500, 500));
+	ui->scrollArea->setWidget(vW);
+
+	ui->scrollArea->setBackgroundRole(QPalette::Dark);
+	ui->scrollArea->setWidgetResizable(true);
+	ui->scrollArea->setHorizontalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+	ui->scrollArea->setVerticalScrollBarPolicy(Qt::ScrollBarAsNeeded);
+
+	vW->setObjectName("ViewerWidget");
+}
+
+//ImageViewer Events
+void ImageViewer::closeEvent(QCloseEvent* event)
+{
+	if (QMessageBox::Yes == QMessageBox::question(this, "Close Confirmation", "Are you sure you want to exit?", QMessageBox::Yes | QMessageBox::No))
+	{
+		event->accept();
+	}
+	else {
+		event->ignore();
+	}
+}
+
+//Image functions
+bool ImageViewer::openImage(QString filename)
+{
+	QImage loadedImg(filename);
+	if (!loadedImg.isNull()) {
+		return vW->setImage(loadedImg); // obr zostane iba ako kopia vo ViewerWidget
+	}
+	return false;
+}
+bool ImageViewer::saveImage(QString filename)
+{
+	QFileInfo fi(filename);
+	QString extension = fi.completeSuffix();
+
+	QImage* img = vW->getImage();
+	return img->save(filename, extension.toStdString().c_str());
+}
+
+bool ImageViewer::invertColors()
+{
+	if (vW->isEmpty()) {
+		return false;
+	}
+
+	uchar* data = vW->getData(); //kopia smernika na data obr
+
+	//PGM Grayscale -> 1 byte = 8 bitov, RGB viac bitov - trojice, stvorice
+
+	int row = vW->getImage()->bytesPerLine(); //bytes in row a aky farbeny model
+	int depth = vW->getImage()->depth();
+
+	for (int i = 0; i < vW->getImgHeight(); i++)
+	{
+		for (int j = 0; j < vW->getImgWidth(); j++)
+		{
+			//Grayscale -> na cviceni hlavne 1 kanal obr (povinne)
+			if (depth == 8) {
+				// function of viewerImage -> controla + nastavenie hodnoty
+				vW->setPixel(j, i, static_cast<uchar>(255 - data[i * row + j]));
+			}
+			//RGBA
+			else {
+				uchar r = static_cast<uchar>(255 - data[i * row + j * 4]);
+				uchar g = static_cast<uchar>(255 - data[i * row + j * 4 + 1]);
+				uchar b = static_cast<uchar>(255 - data[i * row + j * 4 + 2]);
+				vW->setPixel(j, i, r, g, b);
+			}
+		}
+	}
+	vW->update(); // prekreslenie ViewerImage
+	return true;
+}
+
+//Slots
+void ImageViewer::on_actionOpen_triggered()
+{
+	QString folder = settings.value("folder_img_load_path", "").toString();
+
+	QString fileFilter = "Image data (*.bmp *.gif *.jpg *.jpeg *.png *.pbm *.pgm *.ppm .*xbm .* xpm);;All files (*)";
+	QString fileName = QFileDialog::getOpenFileName(this, "Load image", folder, fileFilter);
+	if (fileName.isEmpty()) { return; }
+
+	QFileInfo fi(fileName);
+	settings.setValue("folder_img_load_path", fi.absoluteDir().absolutePath());
+
+	if (!openImage(fileName)) {
+		msgBox.setText("Unable to open image.");
+		msgBox.setIcon(QMessageBox::Warning);
+		msgBox.exec();
+	}
+}
+void ImageViewer::on_actionSave_as_triggered()
+{
+	QString folder = settings.value("folder_img_save_path", "").toString();
+
+	QString fileFilter = "Image data (*.bmp *.gif *.jpg *.jpeg *.png *.pbm *.pgm *.ppm .*xbm .* xpm);;All files (*)";
+	QString fileName = QFileDialog::getSaveFileName(this, "Save image", folder, fileFilter);
+	if (!fileName.isEmpty()) {
+		QFileInfo fi(fileName);
+		settings.setValue("folder_img_save_path", fi.absoluteDir().absolutePath());
+
+		if (!saveImage(fileName)) {
+			msgBox.setText("Unable to save image.");
+			msgBox.setIcon(QMessageBox::Warning);
+		}
+		else {
+			msgBox.setText(QString("File %1 saved.").arg(fileName));
+			msgBox.setIcon(QMessageBox::Information);
+		}
+		msgBox.exec();
+	}
+}
+void ImageViewer::on_actionExit_triggered()
+{
+	this->close();
+}
+
+void ImageViewer::on_actionInvert_triggered()
+{
+	invertColors();
+}
